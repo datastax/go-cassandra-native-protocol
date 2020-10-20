b7b6ab3ca4aca64f123fff72646f8f1df8b370ac
Alexandre Dutra <adutra@users.noreply.github.com>
2020-10-20 16:45:32 +0200

    Fix LZ4 compression logic to handle incompressible messages

--- cassandraprotocol/compression/lz4.go
+++ cassandraprotocol/compression/lz4.go
@@ -2,22 +2,16 @@ package compression
 
 import (
 	"bytes"
-	"errors"
 	"github.com/pierrec/lz4"
 )
 
 type Lz4Compressor struct{}
 
 func (l Lz4Compressor) Compress(uncompressedMessage *bytes.Buffer) (*bytes.Buffer, error) {
-	uncompressedLength := uncompressedMessage.Len()
-	compressedMessage := make([]byte, uncompressedLength)
-	ht := make([]int, 64<<10) // buffer for the compression table
-	n, err := lz4.CompressBlock(uncompressedMessage.Bytes(), compressedMessage, ht)
+	compressedMessage := make([]byte, lz4.CompressBlockBound(uncompressedMessage.Len()))
+	n, err := lz4.CompressBlock(uncompressedMessage.Bytes(), compressedMessage, nil)
 	if err != nil {
 		return nil, err
-	}
-	if n >= uncompressedLength {
-		return nil, errors.New("incompressible buffer")
 	}
 	return bytes.NewBuffer(compressedMessage[:n]), nil
 }
