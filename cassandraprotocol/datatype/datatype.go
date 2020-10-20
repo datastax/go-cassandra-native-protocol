f79a77b663657282df75a908156b3aca73e32c0a
Alexandre Dutra <adutra@users.noreply.github.com>
2020-10-20 21:09:47 +0200

    Use datatype.FindCodec method when locating codecs

--- cassandraprotocol/datatype/datatype.go
+++ cassandraprotocol/datatype/datatype.go
@@ -36,8 +36,8 @@ func FindCodec(code cassandraprotocol.DataTypeCode) (Codec, error) {
 func WriteDataType(t DataType, dest io.Writer, version cassandraprotocol.ProtocolVersion) (err error) {
 	if err = primitives.WriteShort(t.GetDataTypeCode(), dest); err != nil {
 		return fmt.Errorf("cannot write data type code %v: %w", t.GetDataTypeCode(), err)
-	} else if codec, found := codecs[t.GetDataTypeCode()]; !found {
-		return fmt.Errorf("cannot find codec for data type %v", t)
+	} else if codec, err := FindCodec(t.GetDataTypeCode()); err != nil {
+		return err
 	} else if err = codec.Encode(t, dest, version); err != nil {
 		return fmt.Errorf("cannot write data type %v: %w", t, err)
 	} else {
@@ -47,8 +47,8 @@ func WriteDataType(t DataType, dest io.Writer, version cassandraprotocol.Protoco
 
 func LengthOfDataType(t DataType, version cassandraprotocol.ProtocolVersion) (length int, err error) {
 	length += primitives.LengthOfShort // type code
-	if codec, found := codecs[t.GetDataTypeCode()]; !found {
-		return -1, fmt.Errorf("cannot find codec for data type %v", t)
+	if codec, err := FindCodec(t.GetDataTypeCode()); err != nil {
+		return -1, err
 	} else if dataTypeLength, err := codec.EncodedLength(t, version); err != nil {
 		return -1, fmt.Errorf("cannot compute length of data type %v: %w", t, err)
 	} else {
@@ -60,8 +60,8 @@ func ReadDataType(source io.Reader, version cassandraprotocol.ProtocolVersion) (
 	var typeCode cassandraprotocol.DataTypeCode
 	if typeCode, err = primitives.ReadShort(source); err != nil {
 		return nil, fmt.Errorf("cannot read data type code: %w", err)
-	} else if codec, found := codecs[typeCode]; !found {
-		return nil, fmt.Errorf("cannot find codec for type code %v", typeCode)
+	} else if codec, err := FindCodec(typeCode); err != nil {
+		return nil, err
 	} else if decoded, err = codec.Decode(source, version); err != nil {
 		return nil, fmt.Errorf("cannot read data type code %v: %w", typeCode, err)
 	} else {
