6e70be24b4e48eb1810ac557632bf007414af43e
João Reis <joao.r.reis@outlook.com>
2020-10-27 19:27:51 +0000

    add rawframe tests

--- cassandraprotocol/frame/codec_test.go
+++ cassandraprotocol/frame/codec_test.go
@@ -59,6 +59,32 @@ func TestFrameEncodeDecode(t *testing.T) {
 	}
 }
 
+func TestRawFrameEncodeDecode(t *testing.T) {
+	codec := NewCodec()
+	tests := []struct {
+		name  string
+		frame *Frame
+		err   error
+	}{
+		{"request", request, nil},
+		{"response", response, nil},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var rawFrame *RawFrame
+			var err error
+			rawFrame, err = codec.ConvertToRawFrame(test.frame)
+			assert.Equal(t, test.err, err)
+			encodedFrame := &bytes.Buffer{}
+			err = codec.EncodeRaw(rawFrame, encodedFrame)
+			assert.Equal(t, test.err, err)
+			decodedFrame, err := codec.DecodeRaw(encodedFrame)
+			assert.Equal(t, test.err, err)
+			assert.Equal(t, rawFrame, decodedFrame)
+		})
+	}
+}
+
 func TestFrameEncodeDecodeWithCompression(t *testing.T) {
 	codecs := map[string]*Codec{
 		"lz4":    NewCodec(WithCompressor(compression.Lz4Compressor{})),
@@ -86,6 +112,75 @@ func TestFrameEncodeDecodeWithCompression(t *testing.T) {
 					}
 				})
 			}
+		})
+	}
+}
+
+func TestRawFrameEncodeDecodeWithCompression(t *testing.T) {
+	codecs := map[string]*Codec{
+		"lz4":    NewCodec(WithCompressor(compression.Lz4Compressor{})),
+		"snappy": NewCodec(WithCompressor(compression.SnappyCompressor{})),
+	}
+	tests := []struct {
+		name  string
+		frame *Frame
+		err   error
+	}{
+		{"request", request, nil},
+		{"response", response, nil},
+	}
+	for algorithm, codec := range codecs {
+		t.Run(algorithm, func(t *testing.T) {
+			for _, test := range tests {
+				t.Run(test.name, func(t *testing.T) {
+					var rawFrame *RawFrame
+					var err error
+					rawFrame, err = codec.ConvertToRawFrame(test.frame)
+					assert.Equal(t, test.err, err)
+					encodedFrame := &bytes.Buffer{}
+					err = codec.EncodeRaw(rawFrame, encodedFrame)
+					assert.Equal(t, test.err, err)
+					decodedFrame, err := codec.DecodeRaw(encodedFrame)
+					assert.Equal(t, test.err, err)
+					assert.Equal(t, rawFrame, decodedFrame)
+				})
+			}
+		})
+	}
+}
+
+func TestConvertToRawFrame(t *testing.T) {
+	codec := NewCodec()
+	tests := []struct {
+		name  string
+		frame *Frame
+		err   error
+	}{
+		{"request", request, nil},
+		{"response", response, nil},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var rawFrame *RawFrame
+			var err error
+			rawFrame, err = codec.ConvertToRawFrame(test.frame)
+			assert.Equal(t, test.err, err)
+			assert.Equal(t, test.frame.Header.StreamId, rawFrame.Header.StreamId)
+			assert.Equal(t, test.frame.Header.Version, rawFrame.Header.Version)
+			if test.frame.Header.TracingRequested {
+				assert.Equal(t, cassandraprotocol.HeaderFlagTracing, rawFrame.Header.Flags & cassandraprotocol.HeaderFlagTracing)
+			} else {
+				assert.Equal(t, 0, rawFrame.Header.Flags & cassandraprotocol.HeaderFlagTracing)
+			}
+			assert.Equal(t, test.frame.Body.Message.GetOpCode(), rawFrame.Header.OpCode)
+			assert.Equal(t, test.frame.Body.Message.IsResponse(), rawFrame.Header.IsResponse)
+
+			encodedFrame := &bytes.Buffer{}
+			err = codec.Encode(test.frame, encodedFrame)
+			assert.Equal(t, test.err, err)
+			encodedBody := encodedFrame.Bytes()[9:]
+			assert.Equal(t, encodedBody, rawFrame.Body)
+			assert.Equal(t, int32(len(encodedBody)), rawFrame.Header.BodyLength)
 		})
 	}
 }
