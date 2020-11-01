afc327ed80182db4b95968c35c5bcc04a360dec1
Alexandre Dutra <adutra@users.noreply.github.com>
2020-11-01 15:20:57 +0100

    Create struct to hold reason map entries

--- primitive/reasonmap.go
+++ primitive/reasonmap.go
@@ -1,7 +1,6 @@
 package primitive
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -10,52 +9,52 @@ import (
 // <reasonmap> is a map of endpoint to failure reason codes, available from Protocol Version 5 onwards.
 // The map is encoded starting with an [int] n followed by n pairs of <endpoint><failurecode> where
 // <endpoint> is an [inetaddr] and <failurecode> is a [short].
-// Note that [inetaddr] is used as a map key, and is rendered as string since net.IP is not a valid map key type.
+// Note: a reasonmap is inherently a map, but it is not modeled as a map in Go because [inetaddr]
+// is not a valid map key type.
 
-func ReadReasonMap(source io.Reader) (map[string]ReasonMapFailureCode, error) {
+type FailureReason struct {
+	Endpoint net.IP
+	Code     FailureCode
+}
+
+func ReadReasonMap(source io.Reader) ([]*FailureReason, error) {
 	if length, err := ReadInt(source); err != nil {
 		return nil, fmt.Errorf("cannot read reason map length: %w", err)
 	} else {
-		reasonMap := make(map[string]uint16, length)
+		reasonMap := make([]*FailureReason, length)
 		for i := 0; i < int(length); i++ {
 			if addr, err := ReadInetAddr(source); err != nil {
-				return nil, fmt.Errorf("cannot read reason map key: %w", err)
+				return nil, fmt.Errorf("cannot read reason map key for element %d: %w", i, err)
 			} else if code, err := ReadShort(source); err != nil {
-				return nil, fmt.Errorf("cannot read reason map value: %w", err)
+				return nil, fmt.Errorf("cannot read reason map value for element %d: %w", i, err)
 			} else {
-				reasonMap[addr.String()] = code
+				reasonMap[i] = &FailureReason{addr, code}
 			}
 		}
 		return reasonMap, err
 	}
 }
 
-func WriteReasonMap(reasonMap map[string]ReasonMapFailureCode, dest io.Writer) error {
+func WriteReasonMap(reasonMap []*FailureReason, dest io.Writer) error {
 	if err := WriteInt(int32(len(reasonMap)), dest); err != nil {
 		return fmt.Errorf("cannot write reason map length: %w", err)
 	}
-	for addrStr, code := range reasonMap {
-		if addr := net.ParseIP(addrStr); addr == nil {
-			return errors.New("cannot parse reason map key as net.IP: " + addrStr)
-		} else if err := WriteInetAddr(addr, dest); err != nil {
-			return fmt.Errorf("cannot write reason map key: %w", err)
-		} else if err = WriteShort(code, dest); err != nil {
-			return fmt.Errorf("cannot write reason map value: %w", err)
+	for i, reason := range reasonMap {
+		if err := WriteInetAddr(reason.Endpoint, dest); err != nil {
+			return fmt.Errorf("cannot write reason map key for element %d: %w", i, err)
+		} else if err = WriteShort(reason.Code, dest); err != nil {
+			return fmt.Errorf("cannot write reason map value for element %d: %w", i, err)
 		}
 	}
 	return nil
 }
 
-func LengthOfReasonMap(reasonMap map[string]ReasonMapFailureCode) (int, error) {
+func LengthOfReasonMap(reasonMap []*FailureReason) (int, error) {
 	length := LengthOfInt
-	for addrStr := range reasonMap {
-		if addr := net.ParseIP(addrStr); addr == nil {
-			return -1, errors.New("cannot parse IP: " + addrStr)
+	for i, reason := range reasonMap {
+		if inetAddrLength, err := LengthOfInetAddr(reason.Endpoint); err != nil {
+			return -1, fmt.Errorf("cannot compute length of reason map key for element %d: %w", i, err)
 		} else {
-			inetAddrLength, err := LengthOfInetAddr(addr)
-			if err != nil {
-				return -1, fmt.Errorf("cannot compute length reason map key: %w", err)
-			}
 			length += inetAddrLength + LengthOfShort
 		}
 	}
