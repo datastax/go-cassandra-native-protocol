75b2e36abc4bbe5240c0ccbf4d1a747d151adba2
Alexandre Dutra <adutra@users.noreply.github.com>
2020-11-03 16:36:35 +0100

    Improve doc.go file layouts

--- datatype/doc.go
+++ datatype/doc.go
@@ -1,2 +1,7 @@
-// Package datatype contains interfaces, types and functions to read and write CQL data types as defined in section 6 of the CQL protocol specifications.
+/*
+
+Package datatype contains interfaces, types and functions to read and write CQL data types,
+as defined in section 6 of the CQL protocol specifications.
+
+*/
 package datatype
--- message/doc.go
+++ message/doc.go
@@ -1,2 +1,7 @@
-// Package message contains interfaces, types and functions to read and write CQL protocol messages as defined in section 4 of the CQL protocol specifications.
+/*
+
+Package message contains interfaces, types and functions to read and write CQL protocol messages,
+as defined in section 4 of the CQL protocol specifications.
+
+*/
 package message
