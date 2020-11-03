478228df9b855fc14ded534d80f679e8f5571014
Alexandre Dutra <adutra@users.noreply.github.com>
2020-11-03 16:36:35 +0100

    Improve doc.go file layouts

---
+++ client/doc.go
@@ -0,0 +1,11 @@
+/*
+
+Package client contains basic utilities to exchange native protocol frames with compatible endpoints.
+
+The main type in this package is CqlClient, a simple CQL client that can be used to test any CQL-compatible backend.
+
+Please note that code in this package is intended mostly to help driver implementors test their libraries; it should
+not be used in production.
+
+*/
+package client
--- frame/doc.go
+++ frame/doc.go
@@ -1,2 +1,7 @@
-// Package frame contains interfaces, types and functions to read and write CQL protocol frames as defined in sections 1 and 2 of the CQL protocol specifications.
+/*
+
+Package frame contains interfaces, types and functions to read and write CQL protocol frames,
+as defined in sections 1 and 2 of the CQL protocol specifications.
+
+*/
 package frame
